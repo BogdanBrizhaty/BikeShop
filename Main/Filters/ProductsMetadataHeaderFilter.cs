827b15dec60525b595270de7bd022e7157e3feb4
BogdanBrizhaty <demaskinas@yandex.ua>
2017-06-20 19:41:19 +0300

    bug fix for metadata filter

--- Main/Filters/ProductsMetadataHeaderFilter.cs
+++ Main/Filters/ProductsMetadataHeaderFilter.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,9 +11,13 @@ namespace Main.Filters
     {
         public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext)
         {
+            if (!actionExecutedContext.Request.Properties.ContainsKey("X-total-items"))
+                return;
+
             var totalItems = actionExecutedContext.Request.Properties["X-total-items"];
             var totalPages = actionExecutedContext.Request.Properties["X-total-pages"];
             var curPage = actionExecutedContext.Request.Properties["X-current-page"];
+
             actionExecutedContext.Response.Content.Headers.Add("X-total-items", (string)totalItems);
             actionExecutedContext.Response.Content.Headers.Add("X-total-pages", (string)totalPages);
             actionExecutedContext.Response.Content.Headers.Add("X-current-page", (string)curPage);
