83f4b1528c913800cc47d4717f10e94d3076d73f
BogdanBrizhaty <demaskinas@yandex.ua>
2017-06-24 21:47:26 +0300

    auto-active menu button depending on what page is opened, based on angular direcitve

---
+++ Main/Client/Directives/NavigationActivePageHelper.js
@@ -0,0 +1,32 @@
+﻿app.directive('navHelper', ['$location', function ($location) {
+    return {
+        restrict: 'A',
+        scope: false,
+        link: function (scope, element) {
+            function setActive() {
+                var path = $location.path();
+                if (path.match(/\/products\/\d+$/g) !== null) {
+                    angular.forEach(element.find('li'), function (li) {
+                        angular.element(li).removeClass('active');
+                    });
+                    return;
+                }
+                path = (path == '/') ? '/' : '/products';
+
+                if (path) {
+                    angular.forEach(element.find('li'), function (li) {
+                        var anchor = li.querySelector('a');
+                        console.log(anchor.href);
+                        if (anchor.href.match('#!' + path + '(?=\\?|$)')) {
+                            angular.element(li).addClass('active');
+                        } else {
+                            angular.element(li).removeClass('active');
+                        }
+                    });
+                }
+            }
+            setActive();
+            scope.$on('$locationChangeSuccess', setActive);
+        }
+    }
+}]);
