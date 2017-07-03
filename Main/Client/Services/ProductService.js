0524085a7573a955125b610b4f8fcfca89ea7b2c
BogdanBrizhaty <demaskinas@yandex.ua>
2017-07-04 00:05:50 +0300

    Client new ProductService

--- Main/Client/Services/ProductService.js
+++ Main/Client/Services/ProductService.js
@@ -1,45 +1,23 @@
 app.service('ProductService', function ($http) {
     var getProduct = (id) => {
-        $http.get('/api/products/' + id).then((e) => {
-            console.log(e.data);
-            //console.log(e.headers('Content-Type'));
-            return e.data;
-        });
+        return $http.get('/api/products/' + id);
     }
     var getProducts = (page) => {
         page = page >= 1 ? page : 1;
-        $http.get('/api/products/page/' + page).then((e) => {
-            console.log(e.data);
-            return {
-                items: e.headers('X-total-items'),
-                pages: e.headers('X-total-pages'),
-                curPage: e.headers('X-current-page'),
-                products: e.data
-            };
-        });
+        return $http.get('/api/products/page/' + page);
     }
     var findProducts = (q, page) => {
         page = page >= 1 ? page : 1;
-        $http.get('/api/products/search?q=' + q + '&page=' + page).then((e) => {
-            console.log(e.data);
-            return {
-                items: e.headers('X-total-items'),
-                pages: e.headers('X-total-pages'),
-                curPage: e.headers('X-current-page'),
-                products: e.data
-            };
-        });
+        return $http.get('/api/products/search?q=' + q + '&page=' + page);
     }
-    var getTopBuyableProducts = () => {
-        $http.get('/api/products/top').then((e) => {
-            return e.data;
-        });
+    var getMostBuyableProducts = () => {
+        return $http.get('/api/products/top');
     }
 
     return {
         getProduct : getProduct,
         getProducts : getProducts,
         findProducts: findProducts,
-        getTopBuyableProducts: getTopBuyableProducts
+        getMostBuyableProducts: getMostBuyableProducts
     };
 });
