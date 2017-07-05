5be0a9e56e754bdc4b91cccfd45a6805b77e269b
BogdanBrizhaty <demaskinas@yandex.ua>
2017-07-05 14:18:30 +0300

    removing comments

--- Main/Client/Controllers/ProductListController.js
+++ Main/Client/Controllers/ProductListController.js
@@ -5,15 +5,10 @@
             return;
 
         ProductService.getProducts(page).then((e) => {
-            //console.log('opening: ' + page);
             $scope.ProductList = e.data;
-            //console.log($scope.ProductList);
             $scope.currentPage = e.headers('X-current-page');
             $scope.totalPages = e.headers('X-total-pages');
             $scope.totalItems = e.headers('X-total-items');
-            //console.log('Items: ' + $scope.totalItems);
-            //console.log('Pages: ' + $scope.totalPages);
-            //console.log('Cur page: ' + $scope.currentPage);
         })
     };
 
